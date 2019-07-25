1e6841539cea23353d4500a973fea2849dd8abda
Vitalii Pavlenko <pavlusha198806@gmail.com>
2019-07-25 11:10:05 +0300

    second commit

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-var colors = [
-    "red",
-    "green",
-    "blue",
-    "yellow",
-    "lightcoral",
-    "orange",
-    "grey",
-    "black",
-    "darkblue",
-    "darkcyan"    
-];
+var colors = ['yellow', 'blue', 'green', 'red', 'orange', 'purple', 'grey', 'black', 'lightgreen','darkblue'];
 
 var buttonNext = document.querySelector('#Next');
 
@@ -17,21 +6,22 @@ var buttonPrev = document.querySelector('#Prev');
 
 var result = document.querySelector('#result');
 
+var color = 0;
+
 result.style.background = colors[0];
 
-for (var color of colors) {
-    color = 1;
-}
-
 buttonNext.onclick = function () {
-    result.style.background = colors[color++];
-    if(color > 9){
+    color++
+    if (color > colors.length - 1) {
         color = 0;
     }
+    result.style.background = colors[color];
 }
 buttonPrev.onclick = function () {
-    result.style.background = colors[color--];
-    if(color < 0){
+    color--
+    if (color < colors.length - 10) {
         color = 9;
     }
+    result.style.background = colors[color];
+    console.log(color);
 }
